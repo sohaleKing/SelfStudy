b9294bcd0678291065693dbf8d1c84c0804c4019
Bobby Connolly <bobbyconnolly@gmail.com>
2021-11-22 02:18:28 -0500

    add cat wrapper

--- fetch/server/index.js
+++ fetch/server/index.js
@@ -1,8 +1,37 @@
 const express = require("express")
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
 
 const app = express()
 
 app.use(express.static("public"))
+
+app.get("/cat", (req, res) => {
+    const TIMEOUT_MS = 5000
+
+    const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(() => {
+            reject("timeout expired")
+        }, TIMEOUT_MS)
+    })
+
+    const promise = JSDOM.fromURL("https://aws.random.cat/").then((dom) => {
+        const doc = dom.window.document
+        const firstImgTag = doc.documentElement.getElementsByTagName("img")[0]
+        const imageUrl = firstImgTag.getAttribute("src")
+        return imageUrl
+    })
+
+    const arrayOfPromises = [timeoutPromise, promise]
+
+    Promise.race(arrayOfPromises)
+        .then((value) => {
+            res.end(value)
+        })
+        .catch((error) => {
+            res.status(503).end(error)
+        })
+})
 
 app.get("/taco", (req, res) => {
     res.send("hello from taco")
