9104373bb6f9aa4e04ca6761193b38bc0f98fb12
Bobby Connolly <bobbyconnolly@gmail.com>
2021-11-23 21:26:15 -0500

    add class-notes

---
+++ fetch/class-notes.js
@@ -0,0 +1,13 @@
+// These are the same
+
+// Promise.reject("value")
+
+// new Promise((_, reject) => {
+//     reject("value")
+// })
+
+const arrOfStudents = ["Akram", "Claudia", "Konstantin", "Mercy", "Soheyl"]
+
+const ind = Math.floor(Math.random() * 5)
+
+console.log(arrOfStudents[ind])
