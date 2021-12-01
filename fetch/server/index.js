82357e34b99df9733e9502a23bd9348c31db89c3
Bobby Connolly <bobbyconnolly@gmail.com>
2021-11-30 21:28:45 -0500

    lecture 8

--- fetch/server/index.js
+++ fetch/server/index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom
+const cors = require("cors")
 
 const app = express()
 
-app.use(express.static("public"))
+app.use(cors())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+})
 
 app.get("/cat", (req, res) => {
     const TIMEOUT_MS = 200
@@ -27,6 +37,7 @@ app.get("/cat", (req, res) => {
     Promise.race(arrayOfPromises)
         .then((value) => {
             res.end(value)
+            res.json({ data: value })
         })
         .catch((error) => {
             res.status(503).end(error)
