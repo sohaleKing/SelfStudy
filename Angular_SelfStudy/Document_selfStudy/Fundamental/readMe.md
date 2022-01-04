e0b05fdcaeb29bf3acbe37a32a24e0131ab4c5ef
sohaleKing <sohale_r@yahoo.com>
2022-01-04 14:21:16 -0500

    job & self

---
+++ Angular_SelfStudy/Document_selfStudy/Fundamental/readMe.md
@@ -0,0 +1,3 @@
+# create a project `ng new <projectName>`
+
+# create a component `ng generate component <componentName>` or `ng g c <componentName`
---
+++ JobInterview/Angular1/app-module.ts
@@ -0,0 +1,24 @@
+// import { NgModule } from "@angular/core";
+// import { BrowserModule } from "@angular/platform-browser";
+// import { FormsModule } from "@angular/forms";
+
+// import { AppComponent } from "./app.component";
+// import { HelloComponent } from "./hello.component";
+// import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// import { FlexLayoutModule } from "@angular/flex-layout";
+// import { MatButtonModule } from "@angular/material/button";
+// import { MatIconModule } from "@angular/material/icon";
+
+// @NgModule({
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//     BrowserAnimationsModule,
+//     FlexLayoutModule,
+//     MatButtonModule,
+//     MatIconModule,
+//   ],
+//   declarations: [AppComponent, HelloComponent],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
