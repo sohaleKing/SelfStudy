f1a708641b2a7c7d16b45b93bd9f651aaed025b7
sohaleKing <sohale_r@yahoo.com>
2022-01-04 15:21:00 -0500

    disable

--- JobInterview/Angular1/app-module.ts
+++ JobInterview/Angular1/app-module.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from "@angular/core";
+import { interval } from "rxjs";
+import { takeWhile } from "rxjs/operators";
 
 @Component({
   selector: "my-app",
@@ -9,16 +11,32 @@ export class AppComponent {
   //name = 'Angular ' + VERSION.major;
   counter: number;
   isDisabled: boolean;
+  isVisibale: boolean;
+  timerStopped: boolean;
 
   constructor() {
     this.isDisabled = true;
+    this.isVisibale = false;
+    this.counter = 0;
+    this.timerStopped = false;
   }
 
   start() {
     this.isDisabled = false;
+    this.isVisibale = true;
+    this.timerStopped = false;
+    interval(300)
+      .pipe(takeWhile(() => !this.timerStopped))
+      .subscribe(() => {
+        this.counter = this.counter + 1;
+      });
   }
   stop() {
     this.isDisabled = true;
+    this.timerStopped = true;
   }
-  reset() {}
+  reset() {
+    this.counter = 0;
+    this.isVisibale = true;
+  }
 }
