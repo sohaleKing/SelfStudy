985fda06bc73d8397bd0a79c315e31cfc43f374b
sohaleKing <sohale_r@yahoo.com>
2022-02-01 22:55:40 -0500

    add picture

--- Angular_SelfStudy/Practice_selfStudy/addImageURL/src/app/add-image/add-image.component.ts
+++ Angular_SelfStudy/Practice_selfStudy/addImageURL/src/app/add-image/add-image.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-add-image",
   templateUrl: "./add-image.component.html",
-  styleUrls: ["./add-image.component.css"],
+  styles: [],
 })
 export class AddImageComponent implements OnInit {
   constructor() {}
-
+  url: string = "../../assets/img/cat1.jpg";
   addImage() {
-    alert("hi");
+    const img = document.createElement("img");
+    img.setAttribute("src", this.url);
+    const target = document.getElementById("target");
+    target.appendChild(img);
   }
   ngOnInit() {}
 }
--- Angular_SelfStudy/Practice_selfStudy/addImageURL/src/app/app.component.ts
+++ Angular_SelfStudy/Practice_selfStudy/addImageURL/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  templateUrl: `
-<app-add-image></app-add-image>
-    <router-outlet></router-outlet>
-  `,
+  template: `<app-add-image></app-add-image><router-outlet></router-outlet>`,
   styles: [],
 })
 export class AppComponent {
