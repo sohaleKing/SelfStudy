0246f725fde8a0377b90eceb8770a4b28c383402
sohaleKing <sohale_r@yahoo.com>
2022-04-03 23:29:37 -0400

    add component manually

--- Angular_SelfStudy/Practice_selfStudy/pre-workout/src/app/app.module.ts
+++ Angular_SelfStudy/Practice_selfStudy/pre-workout/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular_SelfStudy/Practice_selfStudy/pre-workout/src/app/article/article.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "article",
+  templateUrl: "./article.component.html",
+})
+export class ArticleComponent {}
