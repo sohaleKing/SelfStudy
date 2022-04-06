c82a86c6a72fd3b33ab7701a244bcc5b2df792c8
sohaleKing <sohale_r@yahoo.com>
2022-04-05 22:00:11 -0400

    *ngFor let item of items

--- Angular_SelfStudy/Practice_selfStudy/pre-workout/src/app/nav-bar/nav-bar.component.ts
+++ Angular_SelfStudy/Practice_selfStudy/pre-workout/src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"],
 })
 export class NavBarComponent implements OnInit {
+  navList = ["Home", "About", "Services", "Contact Us"];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
