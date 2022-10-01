4a065161fdb5c260924a021a19a912b3d6363350
sohaleKing <sohale_r@yahoo.com>
2022-10-01 10:14:58 -0400

    mysql to express

--- ExpressJs_selfStudy/Expree_mySql/server.js
+++ ExpressJs_selfStudy/Expree_mySql/server.js
@@ -8,7 +8,7 @@ const mysql = require("mysql2")
 const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
-    password: "LoveGod2021*",
+    password: "*********",
     database: "bdermaplus",
     port: PORT.db,
 })
