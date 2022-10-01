983a77ca35ec19f69a584abc6e4c9b48a4129855
sohaleKing <sohale_r@yahoo.com>
2022-10-01 09:58:03 -0400

    mysql-nodejs:

---
+++ ExpressJs_selfStudy/Expree_mySql/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const PORT = {
+    host: 8080,
+    db: 3306,
+}
+const mysql = require("mysql2")
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "LoveGod2021*",
+    database: "bdermaplus",
+    port: PORT.db,
+})
+connection.connect(function (err) {
+    if (err) {
+        return console.error("error: " + err.message)
+    }
+
+    connection.query("SELECT * FROM customer", function (err, result) {
+        if (err) {
+            return console.error("error: " + err.message)
+        }
+        console.log(result)
+    })
+})
+
+// connection.end()
+app.get("/", (req, res) => {
+    res.json({ message: print.final })
+})
+
+app.listen(PORT.host, () => {
+    console.log("listening on PORT " + PORT.host)
+})
